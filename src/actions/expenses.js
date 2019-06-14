dbb30d8dcce132324e547f826c5ee7cbdecb5def
rdazvd <rdazvd@users.noreply.github.com>
2019-06-14 11:12:27 -0300

    create startSetExpenses action

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -38,3 +38,23 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => (dispatch) => 
+  database.ref('expenses').once('value').then((snapshot) => {
+    const expenses = [];
+
+    snapshot.forEach((childSnapshot) => {
+      expenses.push({
+        id: childSnapshot.key,
+        ...childSnapshot.val()
+      });
+
+      dispatch(setExpenses(expenses));
+    });
+  });
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import { startSetExpenses } from './actions/expenses';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses';
@@ -23,4 +24,8 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => 
+  ReactDOM.render(jsx, document.getElementById('app'))
+);
