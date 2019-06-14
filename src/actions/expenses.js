45216a0114cb53ec30014c09a4e7e6db0fa29444
rdazvd <rdazvd@users.noreply.github.com>
2019-06-14 16:24:22 -0300

    create startRemoveExpense async action

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,6 +32,11 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => 
+  (dispatch) => 
+    database.ref(`expenses/${id}`).remove()
+      .then((ref) => dispatch(removeExpense({ id })));
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
