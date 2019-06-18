c2f78faaf37cd805f93baa71eb170e15ee09e320
rdazvd <rdazvd@users.noreply.github.com>
2019-06-18 13:45:57 -0300

    set up basic authentication action

--- src/app.js
+++ src/app.js
@@ -11,12 +11,9 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-import './firebase/firebase';
+import { firebase } from  './firebase/firebase';
 
 const store = configureStore();
-
-const state = store.getState();
-console.log(getVisibleExpenses(state.expenses, state.filters));
 
 const jsx = (
   <Provider store={store}>
@@ -29,3 +26,11 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 store.dispatch(startSetExpenses()).then(() => 
   ReactDOM.render(jsx, document.getElementById('app'))
 );
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log('log in');
+  } else {
+    console.log('log out');
+  }
+});
